3ef9deb3fc8d3ab196612380b13b7e3e768a898f
Tun-Chieh Huang <thuang39@asu.edu>
2016-09-18 01:04:00 -0700

    Update pipelines.py

--- yelp_crawler/pipelines.py
+++ yelp_crawler/pipelines.py
@@ -11,8 +11,8 @@ class YelpCrawlerPipeline(object):
     	self.input = raw_input("Enter City Again: ")
         self.input = self.input.title().replace(" ","_")+"_Hotel_Review"
         self.csvwriter = csv.writer(open('%s.csv'% (self.input), 'wb'))
-        self.csvwriter.writerow(['hotel_href','hotel_name','hotel_rating','price_range','num_review','review_content','review_content_20','review_content_40','review_content_60','review_content_80','token_list','token_list_20','token_list_40','token_list_60','token_list_80','text_counter','bigram_best','trigram_best'])
+        self.csvwriter.writerow(['hotel_href','hotel_name','hotel_rating','price_range','num_review','review_content','token_list','text_counter','bigram_best','trigram_best'])
 
     def process_item(self, item, yelp):
-        self.csvwriter.writerow([item['hotel_href'].encode('utf-8'),item['hotel_name'].encode('utf-8'),item['hotel_rating'].encode('utf-8'),item['price_range'].encode('utf-8'),item['num_review'].encode('utf-8'),item['review_content'].encode('utf-8'),item['review_content_20'].encode('utf-8'),item['review_content_40'].encode('utf-8'),item['review_content_60'].encode('utf-8'),item['review_content_80'].encode('utf-8'),item['token_list'],item['token_list_20'],item['token_list_40'],item['token_list_60'],item['token_list_80'],item['text_counter'],item['bigram_best'],item['trigram_best']])
+        self.csvwriter.writerow([item['hotel_href'].encode('utf-8'),item['hotel_name'].encode('utf-8'),item['hotel_rating'].encode('utf-8'),item['price_range'].encode('utf-8'),item['num_review'].encode('utf-8'),item['review_content'].encode('utf-8'),item['token_list'],item['text_counter'],item['bigram_best'],item['trigram_best']])
         return item
